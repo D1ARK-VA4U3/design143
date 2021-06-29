e3051fa0ae749446fe87c313c9458f3c50a50b3c
D1ARK-VA4U3 <86417041+D1ARK-VA4U3@users.noreply.github.com>
2021-06-29 21:34:30 +0600

    Update Darkdesign.py

--- Darkdesign.py
+++ Darkdesign.py
@@ -38,7 +38,7 @@ bashrc1 = '''
 clear
 echo
 echo "
-   < ━━━━━━━━━ [★] T E R M U X [★] ━━━━━━━━━━━━ >  " |lolcat
+   < ━━━━━━━━━ [★] D A R K [★] ━━━━━━━━━━━━ >  " |lolcat
 echo
     echo "  We Are AnonyMouse" |lolcat
 '''
@@ -47,7 +47,7 @@ bashrc2 = '''
 echo "
              We Do Not HaCk to impress
                         We HaCk To Express
-   < ━━━━━━━━━━━ [★]BDh@Ckers [★] ━━━━━━━━━━━━ > " |lolcat
+   < ━━━━━━━━━━━ [★]D1ARK-VA4U3 [★] ━━━━━━━━━━━━ > " |lolcat
 
 python /data/data/com.termux/files/usr/etc/wlc.py
 if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
